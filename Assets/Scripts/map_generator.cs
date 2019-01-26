5920a05aa60d98738749312828f03b4c1a67e3b6
grigoriy <gri-bel@yandex.ru>
2019-01-26 19:12:05 +0300

    first commit

---
+++ Assets/Scripts/map_generator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class map_generator : MonoBehaviour
+{
+    public GameObject free_tile;
+    public GameObject wall;
+    public GameObject event_trigger;
+    public float X_start = (float)-3.5, Y_start = (float)-3.5;
+    private int size = 20;
+    public GameObject[,] tiles = new GameObject[20,20]; 
+    public int[,] map_id = new int [20,20] { { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                                             { 1, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                                             { 1, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                                             { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                                             { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                                             { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                                             { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                                             { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                                             { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                                             { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                                             { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                                             { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                                             { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                                             { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                                             { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                                             { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                                             { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                                             { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                                             { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                                             { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }}; // <---- сюда
+    // Start is called before the first frame update
+    void Awake()
+    {
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++) {
+                if(map_id[i,j] == 0)
+                {
+                    tiles[i,j] = Instantiate(free_tile,new Vector3((float)(X_start + (0.3 * i)), (float)(Y_start +  (0.3 * j)), 0),Quaternion.identity);
+                }
+                if (map_id[i, j] == 1)
+                {
+                    tiles[i, j] = Instantiate(wall, new Vector3((float)(X_start + (0.3 * i)), (float)(Y_start + (0.3 * j)), 0), Quaternion.identity);
+                }
+                if (map_id[i, j] == 2)
+                {
+                    tiles[i, j] = Instantiate(event_trigger, new Vector3((float)(X_start + (0.3 * i)), (float)(Y_start + (0.3 * j)), 0), Quaternion.identity);
+                }
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using UnityEngine.Experimental.UIElements;
+
+public class player : MonoBehaviour
+{
+    public GameObject map;
+    public GameObject eventMessage;
+    public GameObject UICanvas;
+    public GameObject UISettings;
+    public Button buttonEMYes;
+    public Button buttonEMNo;
+    public Sprite ass;
+    public Sprite litso;
+    public Sprite pravo;
+    public Sprite levo;
+    public Slider sldr;
+    public Text tStrength;
+    public Text tDexterity;
+    public Text tCute;
+    public int start_tile_x=0, start_tile_y=0;
+    private int cur_tile_x, cur_tile_y;
+    private bool flag_show_event = false;
+    public int hp;
+    public int strength;
+    public int dexterity;
+    public int cute;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<SpriteRenderer>().sprite = litso;
+        eventMessage.SetActive(false);
+        cur_tile_x = start_tile_x;
+        cur_tile_y = start_tile_y;
+        this.transform.position = new Vector3(map.GetComponent<map_generator>().tiles[start_tile_x, start_tile_y].transform.position.x,
+                                              map.GetComponent<map_generator>().tiles[start_tile_x, start_tile_y].transform.position.y,
+                                              (float)(map.GetComponent<map_generator>().tiles[start_tile_x, start_tile_y].transform.position.z + 0.1));
+        hp = PlayerPrefs.GetInt("hp");
+        strength = PlayerPrefs.GetInt("strength");
+        dexterity = PlayerPrefs.GetInt("dexterity");
+        cute = PlayerPrefs.GetInt("cute");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UIHandler();
+        if (Input.GetKeyDown(KeyCode.W)) {
+            if (cur_tile_y < 19 && map.GetComponent<map_generator>().map_id[cur_tile_x, cur_tile_y + 1] != 1 && eventMessage.active == false)
+            {
+                this.GetComponent<SpriteRenderer>().sprite = ass;
+                cur_tile_y++;
+                flag_show_event = false;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.A)) {
+            if (cur_tile_x > 0 && map.GetComponent<map_generator>().map_id[cur_tile_x - 1, cur_tile_y] != 1 && eventMessage.active == false)
+            {
+                this.GetComponent<SpriteRenderer>().sprite = levo;
+                cur_tile_x--;
+                flag_show_event = false;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.S)) {
+            if (cur_tile_y > 0 && map.GetComponent<map_generator>().map_id[cur_tile_x, cur_tile_y - 1] != 1 && eventMessage.active == false)
+            {
+                this.GetComponent<SpriteRenderer>().sprite = litso;
+                cur_tile_y--;
+                flag_show_event = false;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.D)) {
+            if (cur_tile_x < 19 && map.GetComponent<map_generator>().map_id[cur_tile_x + 1, cur_tile_y] != 1 && eventMessage.active == false)
+            {
+                this.GetComponent<SpriteRenderer>().sprite = pravo;
+                cur_tile_x++;
+                flag_show_event = false;
+            }
+        }
+        this.transform.position = new Vector3(map.GetComponent<map_generator>().tiles[cur_tile_x, cur_tile_y].transform.position.x,
+                                             map.GetComponent<map_generator>().tiles[cur_tile_x, cur_tile_y].transform.position.y,
+                                             (float)(map.GetComponent<map_generator>().tiles[cur_tile_x, cur_tile_y].transform.position.z - 0.1));
+        if(flag_show_event == false && map.GetComponent<map_generator>().map_id[cur_tile_x, cur_tile_y] == 2)
+        {
+            flag_show_event = true;
+            eventMessage.SetActive(true);
+        } 
+
+    }
+
+    public void buttonEMNoClick()
+    {
+        eventMessage.SetActive(false);
+    }
+
+    public void buttonUISettings()
+    {
+        UISettings.SetActive(true);
+    }
+
+    public void buttonResumeUISettings()
+    {
+        UISettings.SetActive(false);
+    }
+
+    public void UIHandler()
+    {
+        sldr.value = hp;
+        tStrength.text = strength.ToString();
+        tDexterity.text = dexterity.ToString();
+        tCute.text = cute.ToString();
+}
+}
